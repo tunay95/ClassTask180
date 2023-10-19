c41d231113646a674a983b66e23c36e7d33a664b
Ziver2023 <bd7hskl2t@code.edu.az>
2023-10-19 19:33:17 +0400

    testing

---
+++ ClassTask18/Human.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassTask18
+{
+    class Human
+    {
+        
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+        private int _age;
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value > 0)
+                {
+                    _age = value;
+
+                }
+                else
+                {
+                    throw new AgeException(" Not under 18 years old ! ");
+                   
+                }
+
+            }
+        }
+        public Human(string name,string surname,int age)
+        {
+           Name = name;
+           Surname = surname;
+           Age = age;
+
+
+        }
+    }
+}
+
+
---
+++ ClassTask18/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace ClassTask18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Please enter HUMAN name : ");
+                string input = Console.ReadLine();
+
+                Human human1 = new Human("Tunay", "Huseynli",19);
+                Human human2 = new Human("Ilkin", "Racabov", 19);
+                Human human3 = new Human("Miraga", "Aliyev", 19);
+
+                Human[] arr = { human1, human2, human3 };
+
+                bool found = false;
+                foreach (var item in arr)
+                {
+                    if (item.Name == input)
+                    {
+                        Console.WriteLine(item.Name + " " + item.Surname + " " + item.Age);
+                        found = true;
+                    }
+                }
+                if (!found)
+                {
+                    throw new DataNotFoundException(" Data Not Founded ! ");
+                }
+            }
+            catch (DataNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(AgeException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
